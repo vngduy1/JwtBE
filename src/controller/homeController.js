cd0370c7b52e0abb3ea10a381eddf080ffce84c1
Duy <duyvu372001@gmail.com>
2024-01-06 15:10:38 +0900

    CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,12 +1,55 @@
+import userService from '../service/userService'
+
 const homeController = (req, res) => {
-    res.render('home.ejs')
+  res.render('home.ejs')
 }
 
-const userController = (req, res) => {
-    res.render('user.ejs')
+const userController = async (req, res) => {
+  let userList = await userService.getUserList()
+
+  await userService.deleteUser(4)
+  res.render('user.ejs', { userList })
+}
+
+const createNewUser = (req, res) => {
+  let email = req.body.email
+  let password = req.body.password
+  let username = req.body.username
+
+  userService.createNewUser(email, password, username)
+
+  return res.redirect('/user')
+}
+
+const deleteUser = async (req, res) => {
+  await userService.deleteUser(req.params.id)
+  return res.redirect('/user')
+}
+
+const getUpdateUser = async (req, res) => {
+  let id = req.params.id
+  let user = await userService.getUserById(id)
+  let userData = {}
+  if (user && user.length > 0) {
+    userData = user[0]
+  }
+  return res.render('updateUser.ejs', { userData })
+}
+
+const handleUpdateUser = async (req, res) => {
+  let email = req.body.email
+  let username = req.body.username
+  let id = req.body.id
+  await userService.updateUserInFor(email, username, id)
+
+  return res.redirect('/user')
 }
 
 module.exports = {
-    homeController,
-    userController,
+  homeController,
+  userController,
+  createNewUser,
+  deleteUser,
+  getUpdateUser,
+  handleUpdateUser,
 }
