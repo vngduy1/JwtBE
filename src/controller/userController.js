6a6b3e8f2123b2c4834a228c742b7b2943961dfd
Duy <duyvu372001@gmail.com>
2024-01-16 00:03:36 +0900

    Display_List_Users

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,12 +3,23 @@ import userApiService from '../service/userApiService'
 
 const readUser = async (req, res) => {
   try {
-    let data = await userApiService.getAllUser()
-    return res.status(200).json({
-      EM: data.EM,
-      EC: data.EC,
-      DT: data.DT,
-    })
+    if (req.query.page && req.query.limit) {
+      let page = req.query.page
+      let limit = req.query.limit
+      let data = await userApiService.getUserWithPagination(+page, +limit)
+      return res.status(200).json({
+        EM: data.EM,
+        EC: data.EC,
+        DT: data.DT,
+      })
+    } else {
+      let data = await userApiService.getAllUser()
+      return res.status(200).json({
+        EM: data.EM,
+        EC: data.EC,
+        DT: data.DT,
+      })
+    }
   } catch (error) {
     console.log(e)
     return res.status(500).json({
@@ -43,7 +54,23 @@ const updateUser = (req, res) => {
   }
 }
 
-const deleteUser = (req, res) => {}
+const deleteUser = async (req, res) => {
+  try {
+    let data = await userApiService.deleteUser(req.body.id)
+    return res.status(200).json({
+      EM: data.EM,
+      EC: data.EC,
+      DT: data.DT,
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      EM: 'error from server',
+      EC: -1,
+      DT: '',
+    })
+  }
+}
 
 module.exports = {
   readUser,
