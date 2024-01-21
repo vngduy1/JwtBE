1cf0ce0ccaf235a04c3aedfb69e97d52e36440bd
Duy <duyvu372001@gmail.com>
2024-01-21 13:07:58 +0900

    JwtMiddleware

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ import express from 'express'
 import userController from '../controller/userController'
 import apiController from '../controller/apiController'
 import groupController from '../controller/groupController'
+import { checkUserJWT, checkUserPermission } from '../middleware/JWTAction'
 
 const router = express.Router()
 
@@ -9,15 +10,22 @@ const router = express.Router()
  *
  * @param {*} app init api
  */
-const testMiddleware = (req, res, next) => {
-  console.log('call middleware')
-  next()
-}
+
+// const checkUser = (req, res, next) => {
+//   const nonSecurePaths = ['/', '/register', '/login']
+//   if (nonSecurePaths.includes(req.path)) return next()
+//   //authenticate user
+//   if (user) {
+//     next()
+//   } else {
+//   }
+// }
 
 const initAPiRoutes = (app) => {
-  router.get('/test-api', apiController.testApi)
+  router.all('*', checkUserJWT, checkUserPermission)
+  // router.get('/test-api', apiController.testApi)
   router.post('/register', apiController.handleRegister)
-  router.post('/login', testMiddleware, apiController.handleLogin)
+  router.post('/login', apiController.handleLogin)
 
   router.get('/user/read', userController.readUser)
   router.post('/user/create', userController.createUser)
